9f308abee7a483fcc1111d6acd780e20329f71fb
Gustavo <gustavorr001@gmail.com>
2024-07-13 11:27:52 -0300

    mapping the transfer entity

---
+++ src/main/java/com/gustarodrigues/picpay/entities/Transfer.java
@@ -0,0 +1,34 @@
+package com.gustarodrigues.picpay.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_transfer")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transfer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_sender_id")
+    private Wallet sender;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_receiver_id")
+    private Wallet receiver;
+
+    @Column(name = "value")
+    private BigDecimal value;
+}
