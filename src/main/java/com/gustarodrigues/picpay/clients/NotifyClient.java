5eb26115d0d2312bd6bdacdd5d5a5b4e21b9f8cb
Gustavo <gustavorr001@gmail.com>
2024-07-13 12:08:16 -0300

    creating service that notifies the receipt of the transfer

---
+++ src/main/java/com/gustarodrigues/picpay/clients/NotifyClient.java
@@ -0,0 +1,14 @@
+package com.gustarodrigues.picpay.clients;
+
+import com.gustarodrigues.picpay.entities.Transfer;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(url = "client.notify-service.url")
+public interface NotifyClient {
+
+    @PostMapping
+    ResponseEntity<Void> sendNotify(@RequestBody Transfer transfer);
+}
---
+++ src/main/java/com/gustarodrigues/picpay/services/NotifyService.java
@@ -0,0 +1,32 @@
+package com.gustarodrigues.picpay.services;
+
+import com.gustarodrigues.picpay.clients.NotifyClient;
+import com.gustarodrigues.picpay.entities.Transfer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NotifyService {
+
+    private static final Logger logger = LoggerFactory.getLogger(NotifyService.class);
+
+    private final NotifyClient notifyClient;
+
+    public NotifyService(NotifyClient notifyClient) {
+        this.notifyClient = notifyClient;
+    }
+
+    public void sendNotify(Transfer transfer) {
+        try {
+            logger.info("sending notification");
+
+           var res =  notifyClient.sendNotify(transfer);
+
+           if (res.getStatusCode().isError()) {
+               logger.error("Error while sending notification, status code is not ok");
+           }
+        } catch (Exception e) {
+            logger.error("Error while sending notification", e);
+        }
+    }
+
+}
