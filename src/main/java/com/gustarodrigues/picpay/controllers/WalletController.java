64cb093a99bdbbbdf87fe8ebfc53b4c86c581c9f
Gustavo <gustavorr001@gmail.com>
2024-07-13 10:20:14 -0300

    creating validation of input data and adjusting exception handling

--- src/main/java/com/gustarodrigues/picpay/controllers/WalletController.java
+++ src/main/java/com/gustarodrigues/picpay/controllers/WalletController.java
@@ -3,6 +3,7 @@ package com.gustarodrigues.picpay.controllers;
 import com.gustarodrigues.picpay.controllers.dtos.CreateWalletDto;
 import com.gustarodrigues.picpay.entities.Wallet;
 import com.gustarodrigues.picpay.services.WalletService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +19,7 @@ public class WalletController {
     }
 
     @PostMapping("/wallets")
-    public ResponseEntity<Wallet> createWallet(@RequestBody CreateWalletDto dto) {
+    public ResponseEntity<Wallet> createWallet(@RequestBody @Valid CreateWalletDto dto) {
 
         var wallet = walletService.createWallet(dto);
 
--- src/main/java/com/gustarodrigues/picpay/controllers/dtos/CreateWalletDto.java
+++ src/main/java/com/gustarodrigues/picpay/controllers/dtos/CreateWalletDto.java
@@ -2,12 +2,14 @@ package com.gustarodrigues.picpay.controllers.dtos;
 
 import com.gustarodrigues.picpay.entities.Wallet;
 import com.gustarodrigues.picpay.entities.WalletType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record CreateWalletDto(String fullName,
-                              String document,
-                              String email,
-                              String password,
-                              WalletType.Enum walletType) {
+public record CreateWalletDto(@NotBlank String fullName,
+                              @NotBlank String document,
+                              @NotBlank String email,
+                              @NotBlank String password,
+                              @NotNull WalletType.Enum walletType) {
 
     public Wallet toWallet() {
         return new Wallet(
